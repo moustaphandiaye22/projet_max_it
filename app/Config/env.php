f0534e6065d2698b19ab215806d41d0e6bf5c155
moustaphabdiaye22 <tziprincii434@gmail.com>
2025-07-29 15:56:26 +0000

    mise ajour

--- app/Config/env.php
+++ app/Config/env.php
@@ -9,11 +9,11 @@ if (file_exists(dirname(__DIR__, 2) . '/.env')) {
     $dotenv->load();
 }
 
-define('DB_HOST', $_ENV['DB_HOST'] ?? 'localhost');
-define('DB_NAME', $_ENV['DB_NAME'] ?? 'ges_commercial_auchan');
-define('DB_USER', $_ENV['DB_USER'] ?? 'pguser');
+define('DB_HOST', $_ENV['DB_HOST'] ?? 'aws-0-eu-north-1.pooler.supabase.com');
+define('DB_NAME', $_ENV['DB_NAME'] ?? 'postgres');
+define('DB_USER', $_ENV['DB_USER'] ?? 'postgres.arlexbtjmppdyadcyepw');
 define('DB_PASS', $_ENV['DB_PASS'] ?? 'pgpassword');
-define('APP_URL', $_ENV['APP_URL'] ?? 'https://projet-max-it-hoyl.onrender.com');
+define('APP_URL', $_ENV['APP_URL'] ?? 'http://localhost');
 define('APP_PORT', $_ENV['APP_PORT'] ?? 8000);
 
 
