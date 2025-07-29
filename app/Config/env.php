c838cce614ee3d836390454df2741bcb3b993c3e
moustaphabdiaye22 <tziprincii434@gmail.com>
2025-07-29 00:27:12 +0000

    mise ajour

--- app/Config/env.php
+++ app/Config/env.php
@@ -5,7 +5,9 @@ use Dotenv\Dotenv;
 require_once dirname(__DIR__, 2) . '/vendor/autoload.php';
 
 $dotenv = Dotenv::createImmutable(dirname(__DIR__, 2));
-$dotenv->load();
+if (file_exists(dirname(__DIR__, 2) . '/.env')) {
+    $dotenv->load();
+}
 
 define('DB_HOST', $_ENV['DB_HOST'] ?? 'localhost');
 define('DB_NAME', $_ENV['DB_NAME'] ?? 'ges_commercial_auchan');
